7cf04bf8dd608730535ccd93d48ccd57281c1b51
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2023-12-23 20:31:08 +0200

    Add files via upload

---
+++ empty.py
@@ -0,0 +1,6 @@
+# https://habr.com/ru/articles/754400/
+import sqlite3
+# Создаем подключение к базе данных (файл my_database.db будет создан)
+connection = sqlite3.connect('my_database.db')
+
+connection.close()
